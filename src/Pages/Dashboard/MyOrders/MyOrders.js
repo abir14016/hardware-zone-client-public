6064496692114542924351799994026eea7186d1
abir14016 <abir14016@gmail.com>
2022-05-30 17:50:15 +0600

    final commit

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -13,7 +13,7 @@ const MyOrders = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        fetch(`http://localhost:5000/myorder?email=${email}`, {
+        fetch(`http://localhost:5000/myorders/myorder/${email}`, {
             method: 'GET',
             headers: {
                 'authorization': `Bearer ${localStorage.getItem('accessToken')}`
